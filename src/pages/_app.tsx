86d84092e45850192aa117ac6fc9a2535a0b55cc
Subhan Mahmood <sm01800@surrey.ac.uk>
2023-06-21 20:27:44 +0100

    feat: added form and grouped list

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
-import '@/assets/styles/globals.css';
-
 import theme from '@/assets/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
