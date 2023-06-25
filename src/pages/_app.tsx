95ee4e2cc8b7a8850842ce8bb4b9e8d3e1d89b86
Subhan Mahmood <sm01800@surrey.ac.uk>
2023-06-25 16:00:32 +0100

    feat: added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 import theme from '@/assets/styles/theme';
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
+			<Analytics />
 		</ChakraProvider>
 	);
 }
